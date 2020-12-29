0a004fccfbca63d4bb7025deee33cf092a1d7646
Zdravko Sosa <zdravko.sosa@gmail.com>
2020-12-29 18:10:51 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,55 @@
+import { cocktails } from './menu/cocktails.js';
+import { food } from './menu/food.js';
+import { otherDrinks } from './menu/otherDrinks.js';
+
+const menu = [...cocktails, ...food, ...otherDrinks];
+
+const sectionCenter = document.querySelector('.menu-items');
+const filterBtns = document.querySelectorAll('.filter-btn');
+
+// Load items
+window.addEventListener('DOMContentLoaded', function () {
+  displayMenuItems(food);
+});
+
+// Filter items
+filterBtns.forEach(function (btn) {
+  btn.addEventListener('click', function (e) {
+    const category = e.currentTarget.dataset.id;
+    const menuCategory = menu.filter(function (menuItem) {
+      if (menuItem.category === category) {
+        return menuItem;
+      }
+    });
+    displayMenuItems(menuCategory);
+  });
+});
+
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
+    return `
+    <div class="item-info">
+    <header>
+      <h4 class="text-uppercase">${item.title}</h4>
+      <h4 class="price">${item.price} kn</h4>
+    </header>
+    <p class="item-text">
+    ${item.desc}
+    </p>
+  </div>
+  `;
+  });
+  displayMenu = displayMenu.join('');
+  sectionCenter.innerHTML = displayMenu;
+}
+
+// scroll functions
+$(window).scroll(function (e) {
+  // add/remove class to navbar when scrolling to hide/show
+  var scroll = $(window).scrollTop();
+  if (scroll >= 150) {
+    $('.navbar').addClass('navbar-hide');
+  } else {
+    $('.navbar').removeClass('navbar-hide');
+  }
+});
---
+++ js/menu/food.js
@@ -0,0 +1,101 @@
+export const food = [
+  // BAR FOOD
+
+  {
+    id: 1,
+    title: 'TUNA PATÉ',
+    category: 'bar-food',
+    price: 28,
+    desc: `Tasty, homemade pate made from fresh tuna fish.`,
+  },
+  {
+    id: 2,
+    title: 'BLACK OLIVE TAPENADE',
+    category: 'bar-food',
+    price: 25,
+    desc: `Black olive spread with spices, anchovies and home-made bread.`,
+  },
+  {
+    id: 3,
+    title: 'ANCHOVIE PLATE',
+    category: 'bar-food',
+    price: 35,
+    desc: `Salted or marinated, served on rocket with cappers and home-made bread.`,
+  },
+  {
+    id: 4,
+    title: 'CLUB SANDWICH',
+    category: 'bar-food',
+    price: 32,
+    desc: `Tasty classic combo of ham, cheese, veggies and spreads.`,
+  },
+  {
+    id: 5,
+    title: 'SLIDER BURGERS',
+    category: 'bar-food',
+    price: 40,
+    desc: `Two tasty craft mini-burgers in home-made buns -  our classic.`,
+  },
+  {
+    id: 6,
+    title: 'BRUSCHETTE',
+    category: 'bar-food',
+    price: 40,
+    desc: `Warm snacks inspired by local and seasonal ingredients`,
+  },
+  {
+    id: 7,
+    title: 'CHEESE PLATTER',
+    category: 'bar-food',
+    price: 60,
+    desc: `Selection of local cheeses with jam and home-made bread.`,
+  },
+  {
+    id: 8,
+    title: 'DALMATIAN PLATTER',
+    category: 'bar-food',
+    price: 68,
+    desc: `Dalmatian smoked prosciutto & bratwurst, local cheeses, jam, home made bread.`,
+  },
+
+  // SNACKS
+
+  {
+    id: 9,
+    title: 'Špina snack',
+    category: 'snacks',
+    price: 25,
+    desc: `Sweet & spicy almonds, olives and home made crostini.`,
+  },
+  {
+    id: 10,
+    title: 'Olives',
+    category: 'snacks',
+    price: 12,
+    desc: `Portion of green & black olives with local olive oil.`,
+  },
+  {
+    id: 11,
+    title: 'Home made bread',
+    category: 'snacks',
+    price: 10,
+    desc: `Extra portion.`,
+  },
+
+  // DESSERTS
+
+  {
+    id: 12,
+    title: 'Fig dessert',
+    category: 'desserts',
+    price: 25,
+    desc: `Sweet treat with dried figs, nuts, spices and whipped cream.`,
+  },
+  {
+    id: 13,
+    title: 'Coconut cheesecake',
+    category: 'desserts',
+    price: 25,
+    desc: `Tasty fantasy made of coconut, cream and white chocolate.`,
+  },
+];
