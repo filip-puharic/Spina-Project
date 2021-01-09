26d717b4804eb07d0e365b35cc2d1817cbc60fb7
Zdravko Sosa <zdravko.sosa@gmail.com>
2021-01-10 00:45:36 +0100

    Dropdown menu stays down when clicked

--- js/app.js
+++ js/app.js
@@ -60,3 +60,19 @@ $('[data-fancybox="images"]').fancybox({
   buttons: ['share', 'slideShow', 'close'],
   transitionEffect: 'circular',
 });
+
+// Dropdown stays down when active in menu section
+$(function () {
+  $('.dropdown-toggle').on('click', function (event) {
+    $('.dropdown-menu').slideToggle();
+    event.stopPropagation();
+  });
+
+  $('.dropdown-menu').on('click', function (event) {
+    event.stopPropagation();
+  });
+
+  $(window).on('click', function () {
+    $('.dropdown-menu').slideUp();
+  });
+});
