eab3891c5e2dd257ca929fd1d95df584bf502836
Zdravko Sosa <zdravko.sosa@gmail.com>
2021-01-10 02:42:54 +0100

    Fixed error in console caused by jquery

--- js/app.js
+++ js/app.js
@@ -62,17 +62,7 @@ $('[data-fancybox="images"]').fancybox({
 });
 
 // Dropdown stays down when active in menu section
-$(function () {
-  $('.dropdown-toggle').on('click', function (event) {
-    $('.dropdown-menu').slideToggle();
-    event.stopPropagation();
-  });
-
-  $('.dropdown-menu').on('click', function (event) {
-    event.stopPropagation();
-  });
-
-  $(window).on('click', function () {
-    $('.dropdown-menu').slideUp();
-  });
+$('.dropdown-menu').on('click.bs.dropdown', function (e) {
+  e.stopPropagation();
+  e.preventDefault();
 });
