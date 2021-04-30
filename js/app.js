3e4ff619c91b5ae6e653da73a8a627de6455ea15
Zdravko Sosa <zdravko.sosa@gmail.com>
2021-04-30 21:45:17 +0200

    Revealing elements on scroll

--- js/app.js
+++ js/app.js
@@ -129,3 +129,24 @@ function easeInOutCubic(t, b, c, d) {
 $('.navbar-nav>li>a').on('click', function () {
   $('.navbar-collapse').collapse('hide');
 });
+
+// Reveal sections on scroll
+const allSections = document.querySelectorAll('.section');
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
